42465fd1e66c8c071c8c97b25c5b882e4b99a0ad
Jenary Madia <jenarymadia@gmail.com>
2024-07-22 18:16:41 +0800

    add api route for packages retrieval

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\PackageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/packages', [PackageController::class, 'index']);
