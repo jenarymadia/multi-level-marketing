93f1eaa108f6db061799c9f575d67ae45e209c77
Jenary Madia <ayimadia@Ayis-Macbook.local>
2024-07-22 18:14:12 +0800

    add user binary tree

---
+++ database/migrations/2024_07_21_163903_create_user_tree_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_tree', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->unsignedBigInteger('direct_referral_id')->nullable();
+            $table->enum('position', ['left', 'right'])->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('parent_id')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('direct_referral_id')->references('id')->on('users')->onDelete('set null');
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_tree');
+    }
+};
